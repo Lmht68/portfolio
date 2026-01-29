325f0d80d9a93c4897a8fee7bcc4d5bd2241397d
ThinhLe881 <thinhleminh201@gmail.com>
2026-01-29 01:55:34 -0500

    feat: Add intro section and theme switch button

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import Intro from './components/Intro';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState(() => {
+    if (typeof window !== 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark';
+    }
+    return 'light';
+  });
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(prev => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
+  const sunIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+      />
+    </svg>
+  );
+
+  const moonIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="white"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+      />
+    </svg>
+  );
 
   return (
     <>
-      <div className='flex justify-center items-center'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        type="button"
+        onClick={handleThemeSwitch}
+        className="fixed p-2 z-10 right-20 top-4 bg-violet-300 dark:bg-orange-300 text-lg rounded-md"
+      >
+        {theme === 'dark' ? sunIcon : moonIcon}
+      </button>
+
+      <Parallax pages={4}>
+        <ParallaxLayer
+          offset={0}
+          speed={-0.2}
+          factor={2}
+          className="bg-img-1"
+          style={{ backgroundSize: 'cover' }}
+        />
+
+        <ParallaxLayer
+          offset={0.05}
+          speed={0.05}
+        >
+          <Intro />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2}
+          speed={-0.2}
+          factor={2}
+          className="bg-img-2"
+          style={{ backgroundSize: 'cover' }}
+        />
+      </Parallax>
     </>
   )
 }
---
+++ src/components/Intro.jsx
@@ -0,0 +1,16 @@
+function Intro() {
+    return (
+        <div className="flex items-center justify-center flex-col text-center pt-16 pb-10 bg bg-stone-200 dark:bg-stone-800 text-stone-900 dark:text-stone-100">
+            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Hi, I'm Thinh</h1>
+            <p className="text-base md:text-xl mb-3">Software Developer</p>
+            <p className="mt-6 text-sm max-w-3xl font-medium">
+                I’m a Computer Science graduate from Ontario Tech with hands-on internship experience,
+                building software and systems from idea to execution. I work across software development,
+                embedded systems, data science, and cybersecurity, with a strong focus on problem-solving,
+                adaptability, and writing solid, reliable code.
+            </p>
+        </div>
+    )
+}
+
+export default Intro;
