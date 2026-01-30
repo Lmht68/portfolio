c178594d0228f6b8dd533461ce571da1b039785d
ThinhLe881 <thinhleminh201@gmail.com>
2026-01-30 01:54:54 -0500

    style: Add margin for content blocks

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -3,7 +3,7 @@ import TimelineItem from './TimelineItem';
 
 function Timeline() {
     return (
-        <div className="flex flex-col md:flex-row justify-center p-8 md:p-10 content-block-bg">
+        <div className="flex flex-col md:flex-row justify-center p-5 md:p-10 content-block-bg">
             <div className="w-full md:w-7/12">
                 <h2 className="text-3xl md:text-6xl mb-3 md:mb-8 font-bold text-title">Timeline</h2>
                 {timeline.map((item, index) => (
